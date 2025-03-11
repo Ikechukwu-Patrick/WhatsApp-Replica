009a34380d6e1f05034edf08a9eb2593b7441f01
Ikechukwu-Patrick <patrickikechukwukalukalu@gmail.com>
2025-03-11 17:06:37 +0100

    New env Test 3

--- whatsappclone/src/test/java/com/patrick/whatsappclone/WhatsAppCloneApiApplicationTests.java
+++ whatsappclone/src/test/java/com/patrick/whatsappclone/WhatsAppCloneApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.patrick.whatsappclone;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WhatsAppCloneApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.patrick.whatsappclone;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WhatsAppCloneApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
